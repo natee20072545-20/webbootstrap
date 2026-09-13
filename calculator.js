a9b342ff196e9629f8cbc91c8aadb09000a4b6b3
natee20072545-20 <natee20072545@gmail.com>
2026-09-13 09:22:54 -0700

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function myFunction(x, y) {
+    if (y === undefined) {
+        y = 5;
+    }
+    return x * y;
+}
+function multipy(x) {
+    let num = x;
+    for (let i = 1; i <= 12; i++) {
+        let total = num * i;
+        document.write(num + '*' + i + '=' + total + '<br>');
+    }
+}
